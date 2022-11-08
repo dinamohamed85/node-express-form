0571a8fab1c1affb615075583663d625d07f32f3
dinamohamed85 <dina.elghareeb@gmail.com>
2022-11-08 02:20:48 +0100

    users list added

--- README.md
+++ README.md
@@ -3,6 +3,7 @@ example to submit form with node js and express
 
 - npm init -y
 - npm i express
+- npm install body-parser --save
 - node app.js
 
 
--- app.js
+++ app.js
@@ -1,18 +1,27 @@
 const express = require("express")
 const app = express()
 const port = process.env.PORT || 5000;
+const bodyParser = require('body-parser');
+
+// const users = [{ "id": "1","firstname": "Dina", "lastname": "Mohamed", "email": "dina.elghareeb@gmail.com" }]
+const users = []
 
 app.use(express.static('public'))
-app.use(express.urlencoded({ extended: true }))
-app.use(express.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json())
+
+
 
 app.get("/", (req, res) => {
     res.sendFile(__dirname + '/public/index.html')
 })
 
 app.post("/formpost", (req, res) => {
-    console.log(req.body);
-    res.send(req.body);
+    console.log(req.body.firstName);
+    const { firstname, lastname, email } = req.body;
+    users.push({ id: users.length + 1, firstname: firstname, lastname: lastname, email: email })
+    console.log(users);
+    res.send(users);
 })
 
 app.listen(port, (error) => {
